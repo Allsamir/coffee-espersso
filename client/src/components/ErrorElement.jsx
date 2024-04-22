bcf9262748925d8e7e05ce8fc8f75dddc2da3250
Al Samir <shahinur1591@gmail.com>
2024-04-22 19:57:51 +0600

    Database setup done

---
+++ client/src/components/ErrorElement.jsx
@@ -0,0 +1,21 @@
+import { useRouteError } from "react-router-dom";
+
+const ErrorPage = () => {
+  const error = useRouteError();
+  console.error(error);
+
+  return (
+    <div
+      id="error-page"
+      className="flex justify-center items-center flex-col h-screen"
+    >
+      <h1 className="text-3xl">Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>{error.statusText || error.message}</i>
+      </p>
+    </div>
+  );
+};
+
+export default ErrorPage;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AddCoffee from "./components/AddCoffee.jsx";
+import UpdateCoffee from "./components/UpdateCoffee.jsx";
+import ErrorPage from "./components/ErrorElement.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/add-coffee",
+    element: <AddCoffee />,
+  },
+  {
+    path: "/update-coffee",
+    element: <UpdateCoffee />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
