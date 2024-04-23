403bc7dae29725e804367c6b5ecccf7821a7c2df
Al Samir <shahinur1591@gmail.com>
2024-04-23 21:23:07 +0600

    Full CRUD Operation done

--- client/src/components/CoffeeDetails.jsx
+++ client/src/components/CoffeeDetails.jsx
@@ -1,13 +1,37 @@
 import { useEffect } from "react";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 
 const CoffeeDetails = () => {
   const coffee = useLoaderData();
+  const navigate = useNavigate();
   console.log(coffee);
   useEffect(() => {
     document.title = `Coffee Espersso | ${coffee.coffeeName}`;
   }, [coffee.coffeeName]);
-  return <div>CoffeeDetails</div>;
+  return (
+    <div className="lg:w-1/2 md:w-4/5 w-full px-4 mx-auto my-12">
+      <div className="card bg-base-100 shadow-xl image-full">
+        <figure>
+          <img src={coffee.photoURL} alt="Shoes" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">
+            <span>Name :</span>
+            {coffee.coffeeName}
+          </h2>
+          <p>{coffee.details}</p>
+          <div className="card-actions justify-end">
+            <button
+              onClick={() => navigate(-1)}
+              className="btn btn-outline text-white"
+            >
+              Back
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CoffeeDetails;
